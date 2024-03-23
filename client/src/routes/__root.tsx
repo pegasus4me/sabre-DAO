882ff9848e8f0196f3e15994695a973b4d018c99
pegasus4me <safoantouil@outlook.fr>
2024-03-23 14:59:27 +0100

    web3 connection added

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -5,6 +5,7 @@ export const Route = createRootRoute({
   
     component: () => (
     <>
+
       <div className="p-2 flex gap-2">
         <Link to="/" className="[&.active]:font-bold">
           Home
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,7 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-console.log("dodod", process.env.API_KEY as string)
 export const Route = createFileRoute('/')({
-  component: () => <p>Hello / index</p>
+  component: () => (
+    <>
+    <p>Hello /</p>
+    </>
+  )
   
 })
